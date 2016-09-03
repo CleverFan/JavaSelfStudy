368af06714ea2df4b52e3b99d47cb3a775059187
17706446847 <377109151@qq.com>
2016-09-03 19:46:40 +0800

    add

---
+++ EnumTest/src/com/chengfan/test/OldMethod.java
@@ -0,0 +1,9 @@
+package com.chengfan.test;
+
+/**
+ * Created by ChengFan on 2016/9/3.
+ */
+public class OldMethod {
+    private final static int LEFT = 0;
+    private final static int RIGHT = 1;
+}
